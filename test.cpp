0924deda05244d4dc4378dad84feba03e700dacf
Anirban Chatterjee <ani.kol2099@gmail.com>
2024-11-30 23:42:36 +0530

    C++

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string CharacterName = "Anirban";
+    int CharacterAge;
+    CharacterAge = 23 ;
+
+    cout << "Hello world!" << endl;
+    cout << CharacterName << " is 20 years old"<< endl;
+    cout << "Jai Hind " << endl;
+
+    return 0;
+}
