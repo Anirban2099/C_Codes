f4bb8c9d69d65e8ad3d2e8955bbd02a8075dafe3
Anirban Chatterjee <ani.kol2099@gmail.com>
2025-02-15 23:22:59 +0530

    ok

---
+++ Q2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int b = 25;
+    cout<<"The value of b is: "<<b<<endl;
+    return 0 ;
+
+}
---
+++ Q3.cpp
@@ -0,0 +1,9 @@
+#include<iostream> 
+using namespace std; 
+int main() 
+{ 
+    int b=025; 
+    cout<<"The value of b in decimal is "<<b<<endl; 
+    cout<<"The value of b in octal is "<<oct<<b<<endl; 
+    return(0); 
+    }
