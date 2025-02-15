c31834ff5bf617358f90b0b41a7b8983563dafe7
Anirban Chatterjee <ani.kol2099@gmail.com>
2025-02-16 00:19:21 +0530

    c

---
+++ Q4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int b =0x2B5;
+    cout<<"The value of b in decimal is"<<b<<endl;
+    cout<<"the value of b in hexadecimal is "<<hex<<b<<dec<<endl;
+    cout<<"The value of b in octal is "<<oct<<b<<dec<<endl;
+    return 0;
+}
